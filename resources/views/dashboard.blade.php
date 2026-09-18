9a2652f335b73c8fc7c79e1b5dcab942ec384471
xzidx <c.samnang.de@gmail.com>
2026-09-18 07:51:46 +0700

    set up database

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    @vite('resources/css/app.css')
+
+    <title>Smart Locker</title>
+</head>
+
+<body>
+
+    <x-sidebar />
+
+</body>
+</html>
