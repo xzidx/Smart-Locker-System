dd90fc90ffeacca5b99d4485cfd2abafca71dd30
xzidx <c.samnang.de@gmail.com>
2026-09-21 14:21:00 +0700

    fix the sidebar logo

---
+++ resources/views/locker_usage/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('title', 'locker Usage')
+
+@section('page-title', 'Locker Usage')
+
+@section('page-description', 'Manage your locker usage')
+@section('content')
+
+    <div class="p-8">
+
+        <h2 class="text-2xl font-bold text-[#0B1F3A]">
+            Welcome to the Smart Locker Usage Page
+        </h2>
+
+        <p class="mt-2 text-gray-600">
+            Manage your smart lockers easily from this usage page.
+        </p>
+
+    </div>
+
+@endsection
---
+++ resources/views/lockers/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('title', 'locker')
+
+@section('page-title', 'Lockers')
+
+@section('page-description', 'Manage your lockers')
+@section('content')
+
+    <div class="p-8">
+
+        <h2 class="text-2xl font-bold text-[#0B1F3A]">
+            Welcome to the Smart Locker Lockers Page
+        </h2>
+
+        <p class="mt-2 text-gray-600">
+            Manage your smart lockers easily from this lockers page.
+        </p>
+
+    </div>
+
+@endsection
