6c80664349f635c7be3d110312dae03bbf98f2bd
xzidx <c.samnang.de@gmail.com>
2026-09-21 07:50:05 +0700

    feat/set up table in to database in laraval (It have been done )

--- app/Http/Controllers/LockerUsageController.php
+++ app/Http/Controllers/LockerUsageController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 
 class LockerUsageController extends Controller
 {
-    public function index()
+     public function index()
     {
         $usages = LockerUsage::with('user', 'locker')->get();
 
