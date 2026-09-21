d40d7afd4e49a8386623addd4d6222a742cbfe85
xzidx <c.samnang.de@gmail.com>
2026-09-21 13:23:08 +0700

    Merge branch 'feat/sidebar-component' here im merge the sidebar branch to

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-// use App\Http\Controllers\LocationController;
-// use App\Http\Controllers\LockerController;
-// use App\Http\Controllers\LockerUsageController;
-// use App\Http\Controllers\LockerMaintenanceController;
+use App\Http\Controllers\LocationController;
+use App\Http\Controllers\LockerController;
+use App\Http\Controllers\LockerUsageController;
+use App\Http\Controllers\LockerMaintenanceController;
 
 
 // Dashboard
@@ -14,39 +14,39 @@ Route::get('/dashboard', function () {
 })->name('dashboard');
 
 
-// // Locations
-// Route::resource('locations', LocationController::class);
+// Locations
+Route::resource('locations', LocationController::class);
 
 
-// // Reservation
-// Route::resource('reservation', LockerUsageController::class);
+// Reservation
+Route::resource('reservation', LockerUsageController::class);
 
 
-// // Lockers
-// Route::resource('lockers', LockerController::class);
+// Lockers
+Route::resource('lockers', LockerController::class);
 
 
-// // Locker Usage / Reservation
-// Route::resource('locker_usage', LockerUsageController::class);
+// Locker Usage / Reservation
+Route::resource('locker_usage', LockerUsageController::class);
 
 
-// // Locker Maintenance
-// Route::resource('locker_maintenance', LockerMaintenanceController::class);
+// Locker Maintenance
+Route::resource('locker_maintenance', LockerMaintenanceController::class);
 
 
-// // Settings
-// Route::get('/settings', function () {
-//     return view('settings.index');
-// })->name('settings');
+// Settings
+Route::get('/settings', function () {
+    return view('settings.index');
+})->name('settings');
 
 
-// // Login
-// Route::get('/login', function () {
-//     return view('auth.login');
-// })->name('login');
+// Login
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
 
 
-// // Register
-// Route::get('/register', function () {
-//     return view('auth.register');
-// })->name('register');
+// Register
+Route::get('/register', function () {
+    return view('auth.register');
+})->name('register');
